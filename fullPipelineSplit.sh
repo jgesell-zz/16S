733718ef6098415d3a04ec02378bbe73197093d0
jgesell <gesell@bcm.edu>
2015-11-11 13:19:22 -0600

    Corrected several issues with files calling directories that had been saved as local varibles.

--- fullPipelineSplit.sh
+++ fullPipelineSplit.sh
@@ -71,12 +71,12 @@ for i in {0.4,0.8,1.2,1.6,2.0,2.4,2.8,3.2};
 	mv ${TMPDIR}/uparse{}/temp1.fa ${TMPDIR}/uparse{}/temp.fa;
 done;
 mv ${TMPDIR}/uparse{}/temp.fa ${TMPDIR}/uparse{}/otus1.fa;
-usearch70 -uchime_ref ${TMPDIR}/uparse{}/otus1.fa  -db /users/dpsmith/db/gold/fasta/gold.fa -strand plus -nonchimeras ${TMPDIR}/uparse{}/otus.fa -uchimeout ${TMPDIR}/uparse{}/uchimeref.uc;
-usearch70 -usearch_global ${TMPDIR}/uparse{}/otus.fa -db /users/dpsmith/db/silva/latest/silva_V4.udb -id .968 -strand plus -threads ${THREADS} -uc ${TMPDIR}/uparse{}/otus2taxa.uc -maxaccepts 0 -maxrejects 0;
-cat ${TMPDIR}/uparse{}/derep.fna | grep -A1 "size=1;" | cut -f2 -d ">" | /users/mcwong/getSeq ${TMPDIR}/uparse{}/derep.fna > ${TMPDIR}/uparse{}/singletons.fna;
+usearch70 -uchime_ref ${TMPDIR}/uparse{}/otus1.fa  -db ${GOLD} -strand plus -nonchimeras ${TMPDIR}/uparse{}/otus.fa -uchimeout ${TMPDIR}/uparse{}/uchimeref.uc;
+usearch70 -usearch_global ${TMPDIR}/uparse{}/otus.fa -db ${SILVA}/silva_V4.udb -id .968 -strand plus -threads ${THREADS} -uc ${TMPDIR}/uparse{}/otus2taxa.uc -maxaccepts 0 -maxrejects 0;
+cat ${TMPDIR}/uparse{}/derep.fna | grep -A1 "size=1;" | cut -f2 -d ">" | ${GITREPO}/Miscellaneous/getSeq ${TMPDIR}/uparse{}/derep.fna > ${TMPDIR}/uparse{}/singletons.fna;
 usearch70 -usearch_global ${TMPDIR}/uparse{}/singletons.fna -db ${TMPDIR}/uparse{}/sorted.fa -id .99 -uc ${TMPDIR}/uparse{}/singletons2otus.uc -strand plus -threads ${THREADS} -maxaccepts 32 -maxrejects 128 -minqt 1 -leftjust -rightjust -wordlength 12;
 cd ${TMPDIR}/uparse{};
-/users/mcwong/GitRepos/mcwong/16S_workflows/resolveIterativeUparse.pl ${TMPDIR}/uparse{}/cluster_*.uc ${TMPDIR}/uparse{}/singletons2otus.uc ${TMPDIR}/uparse{}/otus2taxa.uc --derep ${TMPDIR}/uparse{}/derep.uc --chimeras ${TMPDIR}/uparse{}/chimeras.txt --uchime ${TMPDIR}/uparse{}/uchimeref.uc --taxonomy /users/dpsmith/db/silva/latest/silva.map;
+${WONGGITREPO}/16S_workflows/resolveIterativeUparse.pl ${TMPDIR}/uparse{}/cluster_*.uc ${TMPDIR}/uparse{}/singletons2otus.uc ${TMPDIR}/uparse{}/otus2taxa.uc --derep ${TMPDIR}/uparse{}/derep.uc --chimeras ${TMPDIR}/uparse{}/chimeras.txt --uchime ${TMPDIR}/uparse{}/uchimeref.uc --taxonomy ${SILVA}/silva.map;
 biom summarize-table -i ${TMPDIR}/uparse{}/otu_table.biom -o ${TMPDIR}/uparse{}/Stats.{}Merge.otu_table.txt;
 if [ $? -eq 0 ];
 	then echo "biom {} Stats run successful";
@@ -90,7 +90,7 @@ if [ $var -eq 1 ];
 fi;
 cat ${TMPDIR}/uparse{}/Stats.{}Merge.otu_table.txt | tail -n +$var | sed "s/^ //g" | sed -re "s/: /\t/g" | sed "s/\.0$//g" >  ${READSDIR}/../Stats.{}Merge.MappedReads.txt;
 cat  ${READSDIR}/../split_libraries/{}.seqs.fna | grep "^>" | cut -f1 -d "_" | cut -f2 -d ">" | sort | uniq -c >  ${READSDIR}/../Stats.{}Merge.MergedReads.txt;
-perl /users/mcwong/GitRepos/mcwong/16S_workflows/StatsComparisonMergedVsMapped.pl  ${READSDIR}/../Stats.{}Merge.MergedReads.txt  ${READSDIR}/../Stats.{}Merge.MappedReads.txt >  ${READSDIR}/../Stats.{}Merge.Combined.txt;
+perl ${WONGGITREPO}/16S_workflows/StatsComparisonMergedVsMapped.pl  ${READSDIR}/../Stats.{}Merge.MergedReads.txt  ${READSDIR}/../Stats.{}Merge.MappedReads.txt >  ${READSDIR}/../Stats.{}Merge.Combined.txt;
 tar -cvjf  ${READSDIR}/../uparse{}.tar.bz2 ${TMPDIR}/uparse{};
 ' &
 
@@ -107,15 +107,15 @@ cat ${TMPDIR}/temp.fa >> ${TMPDIR}/Reads.fa;
 cat ${TMPDIR}/MergedStandard.fa >> ${TMPDIR}/Reads.fa
 
 #filter francisella
-usearch70 -usearch_global ${TMPDIR}/Reads.fa -db ${FRANCISELLAV4} -strand both -id .968 -uc ${TMPDIR}/Francisella.uc -maxaccepts 0 -maxrejects 0 -threads ${THREADS};
+usearch70 -usearch_global ${TMPDIR}/Reads.fa -db ${FRANCISELLA}/Francisella_V4.udb -strand both -id .968 -uc ${TMPDIR}/Francisella.uc -maxaccepts 0 -maxrejects 0 -threads ${THREADS};
 
 #remove francisella
 cat ${TMPDIR}/Francisella.uc | cut -f9,10 | grep -v "*$" | cut -f1 | cut -f1 -d " " > ${TMPDIR}/Remove;
-cat ${TMPDIR}/MergedStandard.fq | perl /users/gesell/Programs/fastqfilter.pl -v ${TMPDIR}/Remove > ${TMPDIR}/TempMerged.fq &
+cat ${TMPDIR}/MergedStandard.fq | perl ${GITREPO}/Miscellaneous/fastqfilter.pl -v ${TMPDIR}/Remove > ${TMPDIR}/TempMerged.fq &
 pidlist="$pidlist $!"
-cat ${TMPDIR}/Read1.fq | perl /users/gesell/Programs/fastqfilter.pl -v ${TMPDIR}/Remove > ${TMPDIR}/Temp1.fq &
+cat ${TMPDIR}/Read1.fq | perl ${GITREPO}/Miscellaneous/fastqfilter.pl -v ${TMPDIR}/Remove > ${TMPDIR}/Temp1.fq &
 pidlist="$pidlist $!"
-cat ${TMPDIR}/Read2.fq | perl /users/gesell/Programs/fastqfilter.pl -v ${TMPDIR}/Remove > ${TMPDIR}/Temp2.fq &
+cat ${TMPDIR}/Read2.fq | perl ${GITREPO}/Miscellaneous/fastqfilter.pl -v ${TMPDIR}/Remove > ${TMPDIR}/Temp2.fq &
 pidlist="$pidlist $!"
 wait $pidlist
 mv ${TMPDIR}/TempMerged.fq ${TMPDIR}/MergedStandard.fq;
@@ -127,14 +127,14 @@ pbzip2 -p${THREADS} ${TMPDIR}/Read1.fq
 pbzip2 -p${THREADS} ${TMPDIR}/Read2.fq
 pbzip2 -p${THREADS} ${TMPDIR}/MergedStandard.fq
 cat  ${READSDIR}/../SampleList | xargs -I {} mkdir {};
-cat  ${READSDIR}/../SampleList | parallel -j${THREADS} -I {} 'cat ${TMPDIR}/{}.2.fq | perl /users/gesell/Programs/fastqfilter.pl -v ${TMPDIR}/Remove | pbzip2 -p1 -c > {}/{}.2.fq.bz2';
-cat  ${READSDIR}/../SampleList | parallel -j${THREADS} -I {} 'cat ${TMPDIR}/{}.1.fq | perl /users/gesell/Programs/fastqfilter.pl -v ${TMPDIR}/Remove | pbzip2 -p1 -c > {}/{}.1.fq.bz2';
+cat  ${READSDIR}/../SampleList | parallel -j${THREADS} -I {} 'cat ${TMPDIR}/{}.2.fq | perl ${GITREPO}/Miscellaneous/fastqfilter.pl -v ${TMPDIR}/Remove | pbzip2 -p1 -c > {}/{}.2.fq.bz2';
+cat  ${READSDIR}/../SampleList | parallel -j${THREADS} -I {} 'cat ${TMPDIR}/{}.1.fq | perl ${GITREPO}/Miscellaneous/fastqfilter.pl -v ${TMPDIR}/Remove | pbzip2 -p1 -c > {}/{}.1.fq.bz2';
 
 
 
 #recover barcodes for deliverables
-/users/mcwong/GitRepos/mcwong/16S_workflows/recoverBarcodesForRaw.pl ${TMPDIR}/Read1.fq.bz2  ${READSDIR}/../../${PROJECTID}Barcodes/Project_${PROJECTID}/Sample_${PROJECTID}/${PROJECTID}_NoIndex_L001_R2_001.fastq.gz | pbzip2 -p${THREADS} -c > ${TMPDIR}/RawReadsBarcodes.fq.bz2;
-/users/mcwong/GitRepos/mcwong/16S_workflows/recoverBarcodesForRaw.pl ${TMPDIR}/MergedStandard.fq.bz2   ${READSDIR}/../../${PROJECTID}Barcodes/Project_${PROJECTID}/Sample_${PROJECTID}/${PROJECTID}_NoIndex_L001_R2_001.fastq.gz | pbzip2 -p${THREADS} -c > ${TMPDIR}/MergedBarcodes.fq.bz2;
+${WONGGITREPO}/16S_workflows/recoverBarcodesForRaw.pl ${TMPDIR}/Read1.fq.bz2  ${READSDIR}/../../${PROJECTID}Barcodes/Project_${PROJECTID}/Sample_${PROJECTID}/${PROJECTID}_NoIndex_L001_R2_001.fastq.gz | pbzip2 -p${THREADS} -c > ${TMPDIR}/RawReadsBarcodes.fq.bz2;
+${WONGGITREPO}/16S_workflows/recoverBarcodesForRaw.pl ${TMPDIR}/MergedStandard.fq.bz2   ${READSDIR}/../../${PROJECTID}Barcodes/Project_${PROJECTID}/Sample_${PROJECTID}/${PROJECTID}_NoIndex_L001_R2_001.fastq.gz | pbzip2 -p${THREADS} -c > ${TMPDIR}/MergedBarcodes.fq.bz2;
 
 mkdir  ${READSDIR}/../../Deliverables;
 
@@ -149,7 +149,7 @@ cp  ${READSDIR}/../Stats.StrictMerge.Combined.txt  ${READSDIR}/../../Deliverable
 cp  ${READSDIR}/../Stats.StandardMerge.Combined.txt  ${READSDIR}/../../Deliverables/${PROJECTID}.Stats.StandardMerge.Combined.txt;
 cp  ${READSDIR}/../SampleList  ${READSDIR}/../../Deliverables/${PROJECTID}.SampleList;
 cat  ${READSDIR}/../../samplesheet.${PROJECTID}.csv | cut -f3,5 -d "," | tr "," "\t" >  ${READSDIR}/../../Deliverables/${PROJECTID}.SampleSheet.txt;
-head -1 /users/gesell/Projects/IlluminaHeaderExample >  ${READSDIR}/../../Deliverables/${PROJECTID}.ExampleQiimeMappingFile.txt;
+head -1 ${GITREPO}/Miscellaneous/IlluminaHeaderExample >  ${READSDIR}/../../Deliverables/${PROJECTID}.ExampleQiimeMappingFile.txt;
 tail -n+2  ${READSDIR}/../../Deliverables/${PROJECTID}.SampleSheet.txt | sed -re 's/(.*)\t(.*)/\1\t\2\tGGACTACHVGGGTWTCTAAT\tGTGCCAGCMGCCGCGGTAA\t\1/g' >>  ${READSDIR}/../../Deliverables/${PROJECTID}.ExampleQiimeMappingFile.txt;
 chmod -R 777  ${READSDIR}/../../Deliverables;
 
