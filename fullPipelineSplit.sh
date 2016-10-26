cd7b2ff685d2e5f8fba3a9547d8b7020a1319016
jgesell <gesell@bcm.edu>
2016-10-26 16:45:59 -0500

    Corrected potential edge-case with barcode counts file.

--- fullPipelineSplit.sh
+++ fullPipelineSplit.sh
@@ -42,6 +42,7 @@ cat ${READSDIR}/../SampleList | parallel -j${THREADS} -I {} "echo '{} Filter Str
 #concatenate demultiplexed fastqs into monolithic variants
 cat ${TMPDIR}/*.filteredStrict.fq > ${TMPDIR}/seqs.strict.fq &
 cat ${TMPDIR}/*.filteredRaw.fq > ${TMPDIR}/seqs.raw.fq &
+cat ${READSDIR}/../../${PROJECTID}.barcodeCounts.txt | grep -f ${READSDIR}/../SampleList > ${TMPDIR}/${PROJECTID}.barcodeCounts.txt &
 wait;
 
 #filter out phiX bleed
@@ -87,7 +88,7 @@ exit 1;
 fi;
 cat ${TMPDIR}/uparse{}/Stats.{}Merge.otu_table.txt | tail -n +$var | sed "s/^ //g" | sed -re "s/: /\t/g" | sed "s/\.0$//g" > ${READSDIR}/../Stats.{}Merge.MappedReads.txt;
 cat ${READSDIR}/../split_libraries/{}.seqs.fna | grep "^>" | cut -f1 -d "_" | cut -f2 -d ">" | sort | uniq -c > ${READSDIR}/../Stats.{}Merge.MergedReads.txt;
-perl ${GITREPO}/16S/StatsComparisonMergedVsMapped.pl ${READSDIR}/../Stats.{}Merge.MergedReads.txt ${READSDIR}/../Stats.{}Merge.MappedReads.txt ${READSDIR}/../../${PROJECTID}.barcodeCounts.txt > ${READSDIR}/../Stats.{}Merge.Combined.txt;
+perl ${GITREPO}/16S/StatsComparisonMergedVsMapped.pl ${TMPDIR}/${PROJECTID}.barcodeCounts.txt ${READSDIR}/../Stats.{}Merge.MergedReads.txt ${READSDIR}/../Stats.{}Merge.MappedReads.txt > ${READSDIR}/../Stats.{}Merge.Combined.txt;
 tar -cvjf ${READSDIR}/../uparse{}.tar.bz2 -C ${TMPDIR} uparse{};
 ' &
 bigJob=`jobs -p`;
@@ -95,22 +96,22 @@ bigJob=`jobs -p`;
 ## construct the deliverables ##
 #set up francisella filter
 mv ${TMPDIR}/seqs.strict.filtered.fq ${TMPDIR}/Merged_Reads.fq;
-mv ${TMPDIR}/seqs.raw.filtered.fq ${TMPDIR}/Raw_Reads_Temp.fq;
-fq2fa ${TMPDIR}/Raw_Reads_Temp.fq ${TMPDIR}/Raw_Reads_Temp.fa;
+mv ${TMPDIR}/seqs.raw.filtered.fq ${TMPDIR}/Raw_Merged_Reads.fq;
+fq2fa ${TMPDIR}/Raw_Merged_Reads.fq ${TMPDIR}/Raw_Merged_Reads.fa;
 cat ${READSDIR}/../SampleList | parallel -j1 -I {} "cat ${TMPDIR}/{}.1.fq" > ${TMPDIR}/Raw_Read1.fq;
 cat ${READSDIR}/../SampleList | parallel -j1 -I {} "cat ${TMPDIR}/{}.2.fq" > ${TMPDIR}/Raw_Read3.fq;
 cat ${TMPDIR}/Raw_Read1.fq ${TMPDIR}/Raw_Read3.fq > ${TMPDIR}/Raw_Reads.fq;
 fq2fa ${TMPDIR}/Raw_Reads.fq ${TMPDIR}/Raw_Reads.fa;
 usearch70 -fastq_mergepairs ${TMPDIR}/Raw_Read1.fq -reverse ${TMPDIR}/Raw_Read3.fq -fastaout ${TMPDIR}/temp.fa;
 cat ${TMPDIR}/temp.fa >> ${TMPDIR}/Raw_Reads.fa;
-cat ${TMPDIR}/Raw_Reads_Temp.fa >> ${TMPDIR}/Raw_Reads.fa;
+cat ${TMPDIR}/Raw_Merged_Reads.fa >> ${TMPDIR}/Raw_Reads.fa;
 
 #filter francisella
 usearch70 -usearch_global ${TMPDIR}/Raw_Reads.fa -db ${FRANCISELLA}/Francisella_V4.udb -strand both -id .968 -uc ${TMPDIR}/Francisella.uc -maxaccepts 0 -maxrejects 0 -threads ${THREADS};
 
 #remove francisella
 cat ${TMPDIR}/Francisella.uc | cut -f9,10 | grep -v "*$" | cut -f1 | cut -f1 -d " " > ${TMPDIR}/Remove;
-cat ${TMPDIR}/Merged_Reads.fq | perl ${GITREPO}/Miscellaneous/fastqfilter.pl -v ${TMPDIR}/Remove > ${TMPDIR}/TempMerged_Reads.fq &
+cat ${TMPDIR}/Raw_Merged_Reads.fq | perl ${GITREPO}/Miscellaneous/fastqfilter.pl -v ${TMPDIR}/Remove > ${TMPDIR}/TempMerged_Reads.fq &
 cat ${TMPDIR}/Raw_Read1.fq | perl ${GITREPO}/Miscellaneous/fastqfilter.pl -v ${TMPDIR}/Remove > ${TMPDIR}/Temp1.fq &
 cat ${TMPDIR}/Raw_Read3.fq | perl ${GITREPO}/Miscellaneous/fastqfilter.pl -v ${TMPDIR}/Remove > ${TMPDIR}/Temp2.fq &
 for i in `jobs -p | grep -v $bigJob`; do pidlist=`echo -e "\`echo $pidlist\`\n\`echo $((i + 1))\`"`; done
@@ -122,13 +123,12 @@ mv ${TMPDIR}/Temp1.fq ${TMPDIR}/Raw_Read1.fq;
 mv ${TMPDIR}/Temp2.fq ${TMPDIR}/Raw_Read3.fq;
 
 #compress deliverables
-pbzip2 -p${THREADS} ${TMPDIR}/Raw_Read1.fq;
-pbzip2 -p${THREADS} ${TMPDIR}/Raw_Read3.fq;
-pbzip2 -p${THREADS} ${TMPDIR}/Merged_Reads.fq;
-pbzip2 -p${THREADS} ${TMPDIR}/Raw_Reads.fq;
-cat ${READSDIR}/../SampleList | xargs -I {} mkdir {};
-cat ${READSDIR}/../SampleList | parallel -j${THREADS} -I {} 'cat ${TMPDIR}/{}.2.fq | perl ${GITREPO}/Miscellaneous/fastqfilter.pl -v ${TMPDIR}/Remove | pbzip2 -p1 -c > {}/{}.2.fq.bz2';
-cat ${READSDIR}/../SampleList | parallel -j${THREADS} -I {} 'cat ${TMPDIR}/{}.1.fq | perl ${GITREPO}/Miscellaneous/fastqfilter.pl -v ${TMPDIR}/Remove | pbzip2 -p1 -c > {}/{}.1.fq.bz2';
+pbzip2 -f -p${THREADS} ${TMPDIR}/Raw_Read1.fq;
+pbzip2 -f -p${THREADS} ${TMPDIR}/Raw_Read3.fq;
+pbzip2 -f -p${THREADS} ${TMPDIR}/Merged_Reads.fq;
+cat ${READSDIR}/../SampleList | xargs -I {} mkdir -p ${READSDIR}/../../Deliverables/RawSequences/{};
+cat ${READSDIR}/../SampleList | parallel -j${THREADS} -I {} 'cat ${TMPDIR}/{}.2.fq | perl ${GITREPO}/Miscellaneous/fastqfilter.pl -v ${TMPDIR}/Remove | pbzip2 -p1 -c > ${READSDIR}/../../Deliverables/RawSequences/{}/{}.2.fq.bz2';
+cat ${READSDIR}/../SampleList | parallel -j${THREADS} -I {} 'cat ${TMPDIR}/{}.1.fq | perl ${GITREPO}/Miscellaneous/fastqfilter.pl -v ${TMPDIR}/Remove | pbzip2 -p1 -c > ${READSDIR}/../../Deliverables/RawSequences/{}/{}.1.fq.bz2';
 
 #recover barcodes for deliverables
 ${WONGGITREPO}/16S_workflows/recoverBarcodesForRaw.pl ${TMPDIR}/Raw_Read1.fq.bz2 ${READSDIR}/../../${PROJECTID}Barcodes/Project_${PROJECTID}/Sample_${PROJECTID}/${PROJECTID}_NoIndex_L001_R2_001.fastq.gz | pbzip2 -p${THREADS} -c > ${TMPDIR}/Raw_Read2_Barcodes.fq.bz2;
@@ -143,8 +143,9 @@ cp ${READSDIR}/../Stats.StrictMerge.Combined.txt ${READSDIR}/../../Deliverables/
 head -1 ${GITREPO}/Miscellaneous/IlluminaHeaderExample > ${READSDIR}/../../Deliverables/Demultiplex_Sheet.txt;
 cat  ${READSDIR}/../../samplesheet.${PROJECTID}.csv | grep -f ${READSDIR}/../SampleList | cut -f3,5 -d "," | tr "," "\t" | tail -n+1 | sed -re 's/(.*)\t(.*)/\1\t\2\tGGACTACHVGGGTWTCTAAT\tGTGCCAGCMGCCGCGGTAA\t\1/g' >> ${READSDIR}/../../Deliverables/Demultiplex_Sheet.txt;
 cp ${GITREPO}/16S/CMMR16SV4Pipeline.ReadMe.txt ${READSDIR}/../../Deliverables;
-/cmmr/bin/Rscript ~dpsmith/bin/biom2xlsx.r -i ${READSDIR}/../../Deliverables/OTU_table.biom -n ${THREADS} -t ${SILVA}/silva_V4.tre -m ${READSDIR}/../../Deliverables/Demultiplex_Sheet.txt -o ${READSDIR}/../../Deliverables/Summary_Excel_Tables.xlsx
+/cmmr/bin/Rscript /cmmr/bin/deliver_folder.r -f ${READSDIR}/../../Deliverables -t ${SILVA}/silva_V4.tre -n ${THREADS};
 chmod -R 755 ${READSDIR}/../../Deliverables;
+
 
 #return to working directory when script was launched
 cd ${CURRWORKDIR};
